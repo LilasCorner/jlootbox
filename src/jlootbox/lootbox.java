ea380892aa39b605d4167b5f45ace465ed938e63
LilasCorner <59840908+LilasCorner@users.noreply.github.com>
2022-02-24 20:33:02 -0600

    making class names capitalized like they should be

--- src/jlootbox/lootbox.java
+++ src/jlootbox/lootbox.java
@@ -3,10 +3,32 @@
  */
 package jlootbox;
 
+import java.util.Random;
+
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+
 /**
  * @author Lilo
  *
  */
-public class lootbox {
+public class Lootbox {
 
+
+	private String name; 
+	private double value; // current value as calculated 
+	private ContinuousSpace<Object> space; 
+	private Grid<Object> grid;
+	private Random rnd = new Random();
+	
+	
+	
+	
+	@ScheduledMethod(start=1, interval=1)
+	public void step() {
+
+	}
+	
+	
 }
--- src/jlootbox/player.java
+++ src/jlootbox/player.java
@@ -3,10 +3,33 @@
  */
 package jlootbox;
 
+import java.util.List;
+import repast.simphony.engine.watcher.Watch;
+import repast.simphony.engine.watcher.WatcherTriggerSchedule;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.grid.GridPoint;
+import repast.simphony.util.SimUtilities;
+
 /**
  * @author Lilo
  *
  */
-public class player {
+public class Player {
+	
+	
+	
+	
+	//TODO: allows players to keep an
+	@Watch(watcheeClassName = "jlootbox.Player", watcheeFieldNames = "moved",
+			query = "within_moore 1",
+			whenToTrigger = WatcherTriggerSchedule.IMMEDIATE)
+	public void game() {
+		
+
+		
+	}
+	
 
 }
