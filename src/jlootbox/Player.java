33fa0fc26e0f5461cbdcf05af963003c7c46c50b
LilasCorner <59840908+LilasCorner@users.noreply.github.com>
2022-03-02 19:58:19 -0600

    small vers player decision structure built
    need to parameterize certain variables, get jlootboxBuilder created, and player placed on grid

--- src/jlootbox/Player.java
+++ src/jlootbox/Player.java
@@ -3,6 +3,7 @@
  */
 package jlootbox;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 import cern.jet.random.Uniform;
@@ -16,11 +17,12 @@ import repast.simphony.random.RandomHelper;
 public class Player {
 	
 	//maybe we make personal traits/risk factors an array of 1s/0s - 1 if in risk zone 0 if not
-	int availableMoney;  
-	int buyThreshold;
-	Stack<Integer> hist = new Stack<Integer>();
+	private int availableMoney;  
+	private int buyThreshold;
+	private Stack<Integer> hist = new Stack<Integer>();
 	private Uniform unigen = RandomHelper.createUniform(0, 10);
-
+	private int changeRate = 1;
+	
 	
 	public Player(int availMoney, int buy) {
 		this.availableMoney = availMoney;
@@ -28,6 +30,14 @@ public class Player {
 	}
 	
 	
+	public void changeThreshold(int change) {
+		
+		if(buyThreshold + change < 9 && buyThreshold + change > 1) {
+			buyThreshold += change;
+		}
+		
+		return;
+	}
 	
 	//TODO: allows players to keep an
 	@ScheduledMethod(start=1, interval=1)
@@ -35,15 +45,21 @@ public class Player {
 		
 		if(unigen.nextInt() <= buyThreshold) {
 			Lootbox loot = new Lootbox();
-			hist.push(loot.getRarity());
 			
 			if(hist.size() > 1) {//make sure this isn't the first lootbox pulled
 				
-				
+		        if (hist.peek() > loot.getRarity()) { //if new loot worse than last one
+		        	changeThreshold(-1 * changeRate);
+		        }
+		        else {
+		        	changeThreshold(changeRate);
+		        }
 				
 				
 			}
 			
+			hist.push(loot.getRarity());
+
 		}
 
 		
